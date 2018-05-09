78572b3c02128df031c56e894020564cf9bf118a
Leonardo Arias Paz <larias@bse.com.ar>
2018-05-09 10:23:33 -0300

    ABM de cuentas. Clase para extensiones de DataGridView.
    Diseño inicial del ABM de cuentas. Se agrega un método de extensión que permite posicionarse en una fila determinada de un DataGridView.

---
+++ src/Finanzas/Lib/Extensions/DataGridViewExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finanzas.Lib.Extensions
+{
+    public static class DataGridViewExtensions
+    {
+        public static void Posicionar(this DataGridView grid, Func<DataGridViewRow, bool> condición)
+        {
+            int rowIndex = -1;
+            foreach (DataGridViewRow row in grid.Rows)
+            {
+                if (condición(row))
+                {
+                    rowIndex = row.Index;
+                    break;
+                }
+            }
+            if (rowIndex >= 0)
+            {
+                grid.CurrentCell = grid.Rows[rowIndex].Cells[0];
+            }
+        }
+    }
+}
