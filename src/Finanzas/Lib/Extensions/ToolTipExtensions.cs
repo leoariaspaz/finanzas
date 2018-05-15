947371b24219a665b19d7407f949277385f10cfe
Leonardo Arias Paz <larias@bse.com.ar>
2018-05-15 08:19:30 -0300

    ToolTip para mostrar mensajes en TextBox

---
+++ src/Finanzas/Lib/Extensions/ToolTipExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finanzas.Lib.Extensions
+{
+    class ToolTipExtensions
+    {
+        public static void Show(this ToolTip toolTip, IWin32Window window, TextBox textBox, string message)
+        {
+            Point p = new Point(textBox.Left + textBox.Size.Width / 2, textBox.Top + textBox.Size.Height / 2 - 20);
+            if (!textBox.Multiline) p.Y -= 25;
+            p = textBox.FindForm().PointToClient(textBox.Parent.PointToScreen(p));
+            var tt = new ToolTip();
+            tt.IsBalloon = true;
+            tt.ToolTipIcon = ToolTipIcon.Error;
+            tt.ToolTipTitle = "Error";
+            tt.Show(message, window, p, 3000);
+        }
+    }
+}
